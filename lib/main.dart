23cefce6c2b17a132eb233704f924fb504af31ce
Fabrício Cavalcante <fabriciolimacavalcante@gmail.com>
2021-12-08 17:47:59 -0300

    Rework na UI

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class _HomeState extends State<Home> {
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-
   void _resetField() {
     msgController.text = "";
     _url = "";
@@ -31,11 +30,12 @@ class _HomeState extends State<Home> {
       _formKey = GlobalKey<FormState>();
     });
   }
+
   String phoneNumber;
   String phoneIsoCode;
 
-  void onPhoneNumberChange(String number, String internationalizedPhoneNumber,
-      String isoCode) {
+  void onPhoneNumberChange(
+      String number, String internationalizedPhoneNumber, String isoCode) {
     setState(() {
       phoneNumber = number;
       phoneIsoCode = isoCode;
@@ -47,13 +47,14 @@ class _HomeState extends State<Home> {
 
   void _iniciaConversa() async {
     String msgSemEspaco = msgController.text.replaceAll(" ", "%20");
+    var encode = Uri.encodeFull(msgSemEspaco);
     String numero = _numero;
     String url = 'https://wa.me/';
 
     if (msgSemEspaco.isEmpty) {
       url = 'https://wa.me/$numero';
     } else {
-      url = 'https://wa.me/$numero?text=$msgSemEspaco';
+      url = 'https://wa.me/$numero?text=$encode';
     }
 
     if (await canLaunch(url)) {
@@ -62,15 +63,17 @@ class _HomeState extends State<Home> {
       throw 'Não foi possivel iniciar $url';
     }
   }
-  void _link() async{
+
+  void _link() async {
     String msgSemEspaco = msgController.text.replaceAll(" ", "%20");
+    var encode = Uri.encodeFull(msgSemEspaco);
     String numero = _numero;
     String link = 'https://wa.me/';
 
     if (msgSemEspaco.isEmpty) {
       link = 'https://wa.me/$numero';
     } else {
-      link = 'https://wa.me/$numero?text=$msgSemEspaco';
+      link = 'https://wa.me/$numero?text=$encode';
     }
     setState(() {
       _url = link;
@@ -78,66 +81,52 @@ class _HomeState extends State<Home> {
     print(_url);
   }
 
-
   @override
   Widget build(BuildContext context) {
+    double alturaTela = MediaQuery.of(context).size.height;
+    double larguraTela = MediaQuery.of(context).size.width;
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Center(
           child: Image.asset('assets/images/logo.png',
-            fit: BoxFit.contain,
-            width: 256),
+              fit: BoxFit.contain, width: 256),
         ),
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.greenAccent[400],
         elevation: 0,
         actions: <Widget>[
           IconButton(onPressed: _resetField, icon: Icon(Icons.refresh))
         ],
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(32))
-        ),
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(32))),
       ),
-      backgroundColor: Colors.grey[300],
+      /*backgroundColor: Colors.grey[300],*/
       body: Center(
         //formulario
-        child: Container(
-          padding: EdgeInsets.only(top: 60),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/wpp.png"),
-              fit: BoxFit.contain,
-              repeat: ImageRepeat.repeat,
-            ),
-          ),
+        child: SingleChildScrollView(
           child: Form(
             key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.only(top: 30),
                   alignment: Alignment.centerLeft,
                   width: 400,
                   height: 100,
                   child: IntlPhoneField(
                     keyboardType: TextInputType.number,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                     decoration: InputDecoration(
-                      contentPadding: EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 0),
                       hintText: 'Número com DDD: ',
                       hintStyle: TextStyle(color: Colors.grey[600]),
                       filled: true,
-                      fillColor: Colors.white70,
+                      fillColor: Colors.white54,
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
                         borderSide: BorderSide(color: Colors.grey),
                       ),
                       focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                        borderSide: BorderSide(color: Colors.black),
+                        borderSide: BorderSide(color: Colors.greenAccent[400]),
                       ),
                     ),
                     textAlign: TextAlign.center,
@@ -152,83 +141,51 @@ class _HomeState extends State<Home> {
                 ),
                 //formulario mensagem
                 Container(
-                  /*alignment: Alignment.center,*/
                   width: 400,
-                  /*height: 150,*/
-                  child: TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      hintText: 'Mensagem: ',
-                      contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                      hintStyle: TextStyle(color: Colors.grey[600]),
-                      filled: true,
-                      fillColor: Colors.white70,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                    ),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.black, fontSize: 15.0),
-                    controller: msgController,
-                  ),
+                  height: alturaTela * 0.2,
+                  child: buildTextField(
+                      "Mensagem: ", msgController, TextInputType.multiline),
                 ),
+                SizedBox(height: alturaTela * 0.01),
                 //botão enviar
                 Container(
-                  alignment: Alignment.center,
-                  width: 400,
-                  height: 80,
-                  child: ElevatedButton(
-                    child: Text('INICIAR CONVERSA'),
-                    onPressed: _iniciaConversa,
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.greenAccent[400],
-                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                      textStyle:
-                      TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                    alignment: Alignment.center,
+                    width: 400,
+                    /*height: 80,*/
+                    child: buildElevatedButton(
+                        "INICIAR CONVERSA", _iniciaConversa, 30, 20)
                 ),
+                SizedBox(height: alturaTela * 0.01),
                 Container(
                   alignment: Alignment.center,
                   width: 400,
-                  height: 60,
-                  child: ElevatedButton(
-                    child: Text('GERAR LINK'),
-                    onPressed: _link,
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.greenAccent[400],
-                      padding: EdgeInsets.symmetric(horizontal: 55, vertical: 20),
-                      textStyle:
-                      TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                  /*height: 60,*/
+                  child: buildElevatedButton("GERAR LINK", _link, 55, 20),
                 ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SelectableText(_url,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                        showCursor: false,
-                        toolbarOptions: ToolbarOptions(
-                            copy: true,
-                            selectAll: true,
-                            cut: false,
-                            paste: false
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SelectableText(
+                          _url,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          showCursor: false,
+                          enableInteractiveSelection: true,
+                          toolbarOptions: ToolbarOptions(
+                              copy: true,
+                              selectAll: true,
+                              cut: false,
+                              paste: false),
                         ),
-                      ),
-                      /*ElevatedButton(
+                        /*ElevatedButton(
                         onPressed: (){
                         final data = ClipboardData(text: copiarLink.text);
                         Clipboard.setData(data);
                       },child: Icon(Icons.copy),
                           style: ElevatedButton.styleFrom(
-                            primary: Colors.greenAccent[400]))*/]
+                            primary: Colors.greenAccent[400]))*/
+                      ]),
                 ),
               ],
             ),
@@ -237,4 +194,42 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
+
+Widget buildTextField(String text, TextEditingController c, keyboard) {
+  return TextField(
+    keyboardType: keyboard,
+    maxLines: null,
+    decoration: InputDecoration(
+      hintText: text,
+      contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+      hintStyle: TextStyle(color: Colors.grey[600]),
+      filled: true,
+      fillColor: Colors.white54,
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.greenAccent[400]),
+      ),
+    ),
+    textAlign: TextAlign.center,
+    style: TextStyle(color: Colors.black, fontSize: 15.0),
+    controller: c,
+  );
+}
+
+Widget buildElevatedButton(String text, Function f, double h, double v) {
+  return ElevatedButton(
+    onPressed: f,
+    child: Text("$text"),
+    style: ElevatedButton.styleFrom(
+      primary: Colors.greenAccent[400],
+      padding: EdgeInsets.symmetric(horizontal: h, vertical: v),
+      textStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          side: BorderSide(color: Colors.greenAccent[400])),
+    ),
+  );
 }
